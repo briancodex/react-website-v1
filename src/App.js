53d895bb395d5239e245f8c3121f937648d307e0
Martin Ferreira <martin.ferreira@e-core.com>
2022-03-11 13:35:42 -0300

    My commit message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
-
+//test
 function App() {
   return (
     <>
