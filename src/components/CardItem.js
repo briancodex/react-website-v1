59e29ca9e209a6c28ac7420e9ac40e16f0a99fc9
ziei <37557946+ziei@users.noreply.github.com>
2022-04-29 07:41:15 -0700

    Troubleshooting attempt 1

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function CardItem(props) {
   return (
     <>
-      <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
+      <li className="cards__item">
+        <Link className="cards__item__link" to={props.path}>
+          <figure className="cards__item__pic-wrap" data-category={props.label}>
             <img
-              className='cards__item__img'
-              alt='Travel Image'
+              className="cards__item__img"
+              alt="Travel Image"
               src={props.src}
             />
           </figure>
-          <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
+          <div className="cards__item__info">
+            <h5 className="cards__item__text">{props.text}</h5>
           </div>
         </Link>
       </li>
