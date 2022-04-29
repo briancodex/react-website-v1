cffd2a4b314c198d4dac1b28e407ad85825889c8
ziei <37557946+ziei@users.noreply.github.com>
2022-04-29 09:30:29 -0700

    App Structure Complete

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import '../../App.css';
-import Cards from '../Cards';
-import HeroSection from '../HeroSection';
-import Footer from '../Footer';
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+import HeroSection from "../HeroSection";
+import Footer from "../Footer";
 
 function Home() {
   return (
--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import '../../App.css';
+import React from "react";
+import "../../App.css";
 
 export default function Products() {
-  return <h1 className='products'>PRODUCTS</h1>;
+  return <h1 className="products">PRODUCTS</h1>;
 }
