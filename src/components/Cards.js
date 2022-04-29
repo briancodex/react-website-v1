7a8cb1461fe1bc7c1d3201f73a68615acd7c517c
ziei <37557946+ziei@users.noreply.github.com>
2022-04-29 09:41:00 -0700

    bugfix 1.0.0

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="./images/img-9.jpg"
+              src="/images/img-9.jpg"
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
               label="Adventure"
               path="/services"
             />
             <CardItem
-              src="./images/img-2.jpg"
+              src="/images/img-2.jpg"
               text="Travel through the Islands of Bali in a Private Cruise"
               label="Luxury"
               path="/services"
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="./images/img-3.jpg"
+              src="/images/img-3.jpg"
               text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
               label="Mystery"
               path="/services"
             />
             <CardItem
-              src="./images/img-4.jpg"
+              src="/images/img-4.jpg"
               text="Experience Football on Top of the Himilayan Mountains"
               label="Adventure"
               path="/products"
             />
             <CardItem
-              src="./images/img-8.jpg"
+              src="/images/img-.jpg"
               text="Ride through the Sahara Desert on a guided camel tour"
               label="Adrenaline"
               path="/sign-up"
