c7dc88ae60291fc58551ead37a77ec31bd357470
Jaqueline Bianco <97712990+jaquezux@users.noreply.github.com>
2022-07-26 14:04:43 +0100

    Accessibility improvement
    Change the image alt to {props.text} instead of 'Travel Image'. It can help the alt text to be different for each image according to text defined in Cards.js file and improve the accessibility, like screen readers.

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -9,7 +9,7 @@ function CardItem(props) {
           <figure className='cards__item__pic-wrap' data-category={props.label}>
             <img
               className='cards__item__img'
-              alt='Travel Image'
+              alt={props.text}
               src={props.src}
             />
           </figure>
