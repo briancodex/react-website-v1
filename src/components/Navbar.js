26834af218ea5fbf2fa166b84be0ca6adbc71ae1
MookieKonster <kidsonoj@gmail.com>
2022-11-30 14:07:57 +0800

    Change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,7 +40,7 @@ function Navbar() {
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                ABOUT U
+                ABOUT US
               </Link>
             </li>
             <li className='nav-item'>
