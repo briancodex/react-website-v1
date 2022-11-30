7a2fb394139d76484380e4d970ff13bb5a0f3729
anthealai <20007279@myrp.edu.sg>
2022-12-01 01:19:43 +0800

    added codes to aboutus.css and js

--- src/components/pages/Aboutus.js
+++ src/components/pages/Aboutus.js
@@ -6,24 +6,33 @@ import './AboutUs.css';
 function AboutUs() {
     
   return (
-    <>
-          <hr style={{ background: 'black', color: 'black', borderColor: 'black', margin:0 }} />
-            {/* Section for flex display picture and paragraph */}
-            
-             <section className="picture">
-                <div class="container b_flex">
-                    <div className="left_row">
-                    <img src="/images/AboutUsMYob.JPG" alt="" className="picture11"></img>
-                       
-                        
-                    </div>
-                    <div className="right_row">
-                            <img src="/images/Aboutus.png" alt="" className="Apicture12"></img>
-                    </div>
-                </div>
-                
-            </section>
-            </>
+    <><>
+      <hr style={{ background: 'black', color: 'black', borderColor: 'black', margin: 0 }} />
+      {/* Section for flex display picture and paragraph */}
+
+      <section className="picture">
+        <div class="container b_flex">
+          <div className="left_row">
+            <img src="/images/AboutUsMYob.JPG" alt="" className="picture11"></img>
+
+
+          </div>
+          <div className="right_row">
+            <img src="/images/Aboutus.png" alt="" className="Apicture12"></img>
+          </div>
+        </div>
+
+      </section>
+    </><section className="header">
+        <div className="header">
+          <div className="text"><h2>Why MYOB?</h2>
+          </div>
+
+          <div className="label">
+                            <label>Because you didn't start a business to do the books.</label>
+                        </div>
+        </div>
+      </section></>
   );
 }
 
