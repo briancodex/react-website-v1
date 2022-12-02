03ea976f979f6c45eca8a169cfdae0bf34fd60ef
anthealai <20007279@myrp.edu.sg>
2022-12-02 10:08:09 +0800

    new update

--- src/components/pages/Aboutus.js
+++ src/components/pages/Aboutus.js
@@ -6,7 +6,7 @@ import './AboutUs.css';
 function AboutUs() {
     
   return (
-    <><>
+    <><><>
       <hr style={{ background: 'black', color: 'black', borderColor: 'black', margin: 0 }} />
       {/* Section for flex display picture and paragraph */}
 
@@ -25,16 +25,41 @@ function AboutUs() {
     </><section className="header">
         <div className="header">
           <div className="text"><h1>Why MYOB?</h1>
-          <div className="label">
-                <label>Because you didn't start a business to do the books.</label>
-                        </div>                       
+            <div className="label">
+              <label>Because you didn't start a business to do the books.</label>
+
+              <div className="para"></div>
+              <p>We're not here to tell you what success shuld be. Maybe you want a day off every week, or more time to go surfing. Maybe you want a worldwide empire. Whatever you want for your business-we can help you achieve it.</p>
+            </div>
+          </div>
         </div>
+
+      </section></><section className="icons">
+        <div className="grid">
+          <div className="item">
+            <i class="fa-solid fa-chart fa-3x">
+            </i>
+            <h2>Multi-Currencies</h2>
+            <p>Process transactions with numerous foreign Currencies at ease for your business with MYOB</p>
+          </div>
           </div>
 
-          
-      </section></>
-  );
-}
+          <div className="item">
+            <i class="fa-solid fa-users fa-3x"></i>
+            <h2>Multi-User</h2>
+            <p>Allows multiple user access the MYOB file simultaneously.</p>
+          </div>
+
+
+
+          );
+
+        </section></>
+
+  
+
+)}
+
 
 export default AboutUs;
             
