488edcdd9f1cedb304561675cefd21492520f674
20017494-Khor-Jinq-Wen <87064030+20017494-Khor-Jinq-Wen@users.noreply.github.com>
2022-12-05 12:48:58 +0800

    Created blog page

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import '../../App.css';
+import './Blog.css';
 
 export default function Blog() {
-  return <h1>Blog</h1>;
+  return (
+    
+    <>
+    <hr style={{ background: 'black', color: 'black', borderColor: 'black', margin:0 }} />
+            {/* Section for flex display picture and paragraph */}
+    <section className='blogPic'>
+        <div className="top_Pic">
+          <img src="/images/blog pic.png" alt="" className="blog_pic"></img>
+        </div>
+    </section>
+    
+    <section className='ourblog'>
+      <div className="blogheader"><b>Our Blog</b></div>
+    </section>
+
+    {/*adding in the blog link*/}
+    <section className='shouldI'>
+      <img src='/images/Blog accounting.png' alt='' className="accounting"></img>
+      <div className="abss"><b>Should I upgrade by ABSS (fka MYOB) Accounting Software?</b></div>
+      
+    </section>
+
+
+      </>
+    );
 }
