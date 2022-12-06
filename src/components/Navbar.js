8a4ffbc1cd114c8cad5d6d97e0932d09af8d8b63
MookieKonster <kidsonoj@gmail.com>
2022-12-06 16:49:35 +0800

    navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -78,7 +78,7 @@ function Navbar() {
 
           </ul>
           {button && <Button buttonStyle='btn--outline'>
-          <Link to='/Psggrant' className='nav-links1' onClick={closeMobileMenu}>
+          <Link to='/' className='nav-links1' onClick={closeMobileMenu}>
           LOG IN </Link></Button>}
 
 
