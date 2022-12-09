bab5b6a6f728c4f864897fb7f48207e8b6061504
MookieKonster <kidsonoj@gmail.com>
2022-12-09 12:25:25 +0800

    navbar overelay

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function Navbar() {
           <Modal.Body>
               <Form>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                  <Form.Label>Email address</Form.Label>
+                  <Form.Label>Email address: </Form.Label>
                   <Form.Control
                     type="email"
                     placeholder="name@example.com"
@@ -105,7 +105,7 @@ function Navbar() {
                     className="mb-3"
                     controlId="exampleForm.ControlTextarea1"
                   >
-                  <Form.Label>Example textarea</Form.Label>
+                  <Form.Label>Example textarea: </Form.Label>
                   <Form.Control as="textarea" rows={3} />
                 </Form.Group>
               </Form>
