644f9e1d14aaf66c2a64ea0d55fe68c707c1ce5c
MookieKonster <kidsonoj@gmail.com>
2022-12-12 13:59:31 +0800

    Nav bar popup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -85,7 +85,7 @@ function Navbar() {
               
 
           </ul>
-          <Button variant="primary" onClick={handleShow}>LOGIN</Button>
+          <Button variant="primary" onClick={handleShow} className="loginbtn">LOGIN</Button>
 
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
@@ -93,23 +93,10 @@ function Navbar() {
           </Modal.Header>
           <Modal.Body>
               <Form>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                  <Form.Label>Email address: </Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="name@example.com"
-                    autoFocus
-                  />
-                  </Form.Group>
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlTextarea1"
-                  >
-                  <Form.Label>Example textarea: </Form.Label>
-                  <Form.Control as="textarea" rows={3} />
-                </Form.Group>
+                
               </Form>
           </Modal.Body>
+
             <Modal.Footer>
               <Button variant="secondary" onClick={handleClose}>
                 Close
