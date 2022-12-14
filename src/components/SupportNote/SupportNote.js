89e0ff62b48a030c8bc282dbc37447501b670e0b
anthealai <20007279@myrp.edu.sg>
2022-12-15 05:02:28 +0800

    ABOUTUStesting

--- src/components/SupportNote/SupportNote.js
+++ src/components/SupportNote/SupportNote.js
@@ -2,5 +2,31 @@ import React from 'react';
 import '../../App.css';
 
 export default function Support() {
-  return <h1>Support Note</h1>;
-}
+
+  return (
+<>
+
+  <section className="Supprtnotepic">
+    <div className="SNpicture">
+    <img src=" /images/SupportNote.png" alt="" className="SNPic"></img>
+    </div>
+  </section>
+
+  <section className="SNHeader">
+        <div className="sn_header">
+          <div className="snhead">
+            <h1>Support Notes</h1>
+          </div>
+          <div className="snlabel">
+            <label>Click on it to find out more about it!</label>
+          </div>
+          </div>
+  </section>
+
+
+
+</>
+  )
+  }
+  
+
