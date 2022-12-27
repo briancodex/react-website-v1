4490013a00f946392a212deff9ecdaa3dca0e841
20046050-Ervin <87052714+20046050-Ervin@users.noreply.github.com>
2022-12-27 22:05:08 +0800

    Softwarepage sorta done

---
+++ src/components/Software/Item.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 650px;
+  width: 300px;
+  background-color: lightgrey;
+  color: #fff;
+  margin: 0 15px;
+  font-size: 4em;
+`;
