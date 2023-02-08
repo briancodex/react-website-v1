9669341d08dd19081aec231fb23fb921da78ae01
20017494-Khor-Jinq-Wen <87064030+20017494-Khor-Jinq-Wen@users.noreply.github.com>
2023-02-08 13:42:02 +0800

    Done with cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -263,7 +263,7 @@ function Navbar() {
               
             </li>
             <li className='nav-item'>
-              <Link to='/Cart' className='nav-links' onClick={closeMobileMenu}
+              <Link to='/Apps' className='nav-links' onClick={closeMobileMenu}
               >
               <i class="fa-solid fa-cart-shopping fa-2x"></i>
               </Link>
